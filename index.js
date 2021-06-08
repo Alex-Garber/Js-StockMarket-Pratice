a4b281aefc6525eadcc671d91386476bdc6e9619
Alex Garber <alexgarber6@gmail.com>
2021-06-08 13:22:40 -0600

    connected to stock api

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const express = require('express')
 const app = express()
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+
+// Public api key pk_e3a65e804c4b49f38d873236fc222daf
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_e3a65e804c4b49f38d873236fc222daf', {json: true}, (err, res, body) => {
+    if(err) {return console.log(err);}
+    console.log(body);
+    if (res.statusCode === 200){
+        console.log(body);
+    };
+});
+
+
 
 //Set handlebars Middleware
 app.engine('handlebars', exphbs());
@@ -19,6 +31,12 @@ app.get('/', function (req, res) {
        stuff: "This is stuff..."
     });
 });
+
+// create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
+});
+
 
 //Set static folder 
 app.use(express.static(path.join(__dirname, 'public')));
