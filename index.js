ea7f42f5aeb6feed7038136bd94e67490f9bfb51
Alex Garber <alexgarber6@gmail.com>
2021-06-09 09:38:27 -0600

    Ticker search is working

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ const app = express()
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+
+//Use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 //connecting to api 
 // Public api key pk_e3a65e804c4b49f38d873236fc222daf
 
 //Create call api function
-function call_api(finishedApi){
-request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_e3a65e804c4b49f38d873236fc222daf', {json: true}, (err, res, body) => {
+function call_api(finishedApi, ticker){
+request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_e3a65e804c4b49f38d873236fc222daf', {json: true}, (err, res, body) => {
     if(err) {return console.log(err);}
     console.log(body);
     if (res.statusCode === 200){
@@ -32,7 +37,9 @@ request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_e3a65e804c4b49
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-//Set handlebar routes 
+
+
+//Set handlebar index GET route 
 app.get('/', function (req, res) {
     call_api(function(doneAPI){
            res.render('home', {
@@ -40,6 +47,19 @@ app.get('/', function (req, res) {
     });
 
     });
+ 
+});
+
+
+//Set handlebar index POST route 
+app.post('/', function (req, res) {
+    call_api(function(doneAPI){
+          // posted_stuff = req.body.stock_lookup;
+           res.render('home', {
+           Stock: doneAPI
+    });
+
+    }, req.body.stock_lookup);
  
 });
 
