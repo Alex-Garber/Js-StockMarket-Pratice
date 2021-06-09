b9b758de22c29edb2ca0d609f474719f26f9eb3d
Alex Garber <alexgarber6@gmail.com>
2021-06-08 18:12:41 -0600

    styleing api data

--- index.js
+++ index.js
@@ -10,14 +10,21 @@ const request = require('request');
 
 const PORT = process.env.PORT || 5000;
 
+
+//connecting to api 
 // Public api key pk_e3a65e804c4b49f38d873236fc222daf
+
+//Create call api function
+function call_api(finishedApi){
 request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_e3a65e804c4b49f38d873236fc222daf', {json: true}, (err, res, body) => {
     if(err) {return console.log(err);}
     console.log(body);
     if (res.statusCode === 200){
-        console.log(body);
+       //onsole.log(body);
+       finishedApi(body);
     };
 });
+};
 
 
 
@@ -27,9 +34,13 @@ app.set('view engine', 'handlebars');
 
 //Set handlebar routes 
 app.get('/', function (req, res) {
-    res.render('home', {
-       stuff: "This is stuff..."
+    call_api(function(doneAPI){
+           res.render('home', {
+           Stock: doneAPI
     });
+
+    });
+ 
 });
 
 // create about page route
